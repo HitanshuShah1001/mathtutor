08669fa567642a47632249657b9412a081f5b560
Hitanshu Shah <hitanshushah5@gmail.com>
2025-01-06 02:15:50 +0530

    base url changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 export const USER = "user";
 export const ASSISTANT = "assistant";
-export const BASE_URL_API = "http://tutor.ap-south-1.elasticbeanstalk.com";
+export const BASE_URL_API = "https://tutor.ap-south-1.elasticbeanstalk.com";
 export const API_SEND_OTP = "auth/send-otp";
 export const OTP_VERIFY = "auth/verify-otp";
 export const ACCESS_KEY = "accessKey";
