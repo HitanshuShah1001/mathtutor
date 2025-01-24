f054c1a430195ecc37cff6b6217571dc7bb2e216
HitanshuShah1001 <hitanshushah5@gmail.com>
2025-01-24 11:13:39 +0530

    s3 utils

--- src/utils/s3utils.js
+++ src/utils/s3utils.js
@@ -1,17 +1,16 @@
 import AWS from "aws-sdk";
 
 const s3 = new AWS.S3();
-
-export async function uploadToS3(content, name) {
-  const fileKey = `questionPapers/${name}.html`;
+l;
+export async function uploadToS3(content, link) {
+  const fileKey = link.split(".com/")[1]; // This wi
   const uploadParams = {
     Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
     Key: fileKey,
     Body: content,
     ContentType: "text/html",
   };
-
   await s3.upload(uploadParams).promise();
-  const fileUrl = `https://${process.env.REACT_APP_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_REGION}.amazonaws.com/${fileKey}`;
+  const fileUrl = link;
   return fileUrl;
 }
